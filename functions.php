dd78c3cbddf87ee03de3fdc9e2642bbb5df26575
AkikoYamamotoBeraud <akikoyamamotoberaud@gmail.com>
2024-08-12 23:09:54 +0200

    contact form

--- functions.php
+++ functions.php
@@ -12,8 +12,13 @@ function custom_theme_setup() {
 add_action('after_setup_theme', 'custom_theme_setup');
 
 function custom_enqueue_scripts() {
-    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=YourFontFamily:wght@400;700&display=swap', false);
     wp_enqueue_style('theme-style', get_stylesheet_uri());
     wp_enqueue_script('theme-scripts', get_template_directory_uri() . '/js/script.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'custom_enqueue_scripts');
+
+function my_theme_enqueue_styles() {
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Space+Mono:wght@400;700&display=swap', false);
+}
+
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
