5fcd3799969b7b6426b62bcafd32a3a1c9152bfa
AkikoYamamotoBeraud <akikoyamamotoberaud@gmail.com>
2024-08-13 12:58:25 +0200

    changing footer aftre

--- footer.php
+++ footer.php
@@ -9,12 +9,6 @@
         ));
         ?>
     </nav>
- <!-- content 
-    <div class="footer-links">
-        <a href="<?php echo esc_url(home_url('/mentions-legales')); ?>">MENTIONS LÉGALES</a>
-        <a href="<?php echo esc_url(get_privacy_policy_url()); ?>">VIE PRIVÉE</a>
-        <a>TOUS DROITS RÉSERVÉS</a>
-    </div>-->
     <?php get_template_part('template-parts/modal', 'contact'); ?>
 </footer>
 </body>
