900b55f5ea04b4ac5e505c0abf15d8cde7530479
AkikoYamamotoBeraud <akikoyamamotoberaud@gmail.com>
2024-08-27 00:45:42 +0200

    filter and hover

--- footer.php
+++ footer.php
@@ -15,13 +15,13 @@
     <div id="lightbox-overlay" class="lightbox-overlay" style="display: none;">
         <div id="lightbox-content" class="lightbox-content">
             <button id="lightbox-close" class="lightbox-close">✖</button>
-            <button id="lightbox-prev" class="lightbox-prev">← Précédente</button>
+            <button id="lightbox-prev" class="lightbox-prev">Précédente</button>
             <img id="lightbox-image" src="" alt="Lightbox Image" />
             <div id="lightbox-info" class="lightbox-info">
                 <p id="lightbox-ref-number" class="lightbox-ref-number"></p>
                 <p id="lightbox-category" class="lightbox-category"></p>
             </div>
-            <button id="lightbox-next" class="lightbox-next">Suivante →</button>
+            <button id="lightbox-next" class="lightbox-next">Suivante</button>
         </div>
     </div>
     <?php wp_footer(); ?>
